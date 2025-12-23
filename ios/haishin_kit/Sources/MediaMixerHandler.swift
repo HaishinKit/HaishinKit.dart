0ad3a3ad2c7816abdee592650f7f2b3962c535cb
kikuchy <kikuchy.h@gmail.com>
2025-12-23 15:18:11 +0900

    fix: guard invalid track number

--- ios/haishin_kit/Sources/MediaMixerHandler.swift
+++ ios/haishin_kit/Sources/MediaMixerHandler.swift
@@ -155,12 +155,16 @@ extension MediaMixerHandler: MethodCallHandler {
                 result(texture?.id)
             }
         case "RtmpStream#attachVideo":
+            let track = arguments["track"] as? UInt8
+            guard let track = track else {
+                result(FlutterError(code: "INVALID_ARGUMENT", message: "track is nil", details: nil))
+                return
+            }
             let source = arguments["source"] as? [String: Any?]
             let id = source?["id"] as? String
-            let track = arguments["track"] as? UInt8
-            guard let id = id, let track = track else {
+            guard let id = id else {
                 Task {
-                    try? await mixer.attachVideo(nil, track: 0)
+                    try? await mixer.attachVideo(nil, track: track)
                     result(nil)
                 }
                 return
