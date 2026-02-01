847c96cbbbf5fa1ce77982c4902f4a42855b70f2
shogo4405 <shogo4405@gmail.com>
2026-02-01 23:18:16 +0900

    fix import error.

--- test/haishin_kit_test.dart
+++ test/haishin_kit_test.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_test/flutter_test.dart';
-import 'package:haishin_kit/haishin_kit.dart';
-import 'package:haishin_kit/haishin_kit_platform_interface.dart';
+import 'package:haishin_kit/platform_interface_haishin_kit.dart';
 import 'package:haishin_kit/method_channel_haishin_kit.dart';
 import 'package:haishin_kit/stream_session_mode.dart';
 import 'package:haishin_kit/video_source.dart';
@@ -37,7 +36,6 @@ void main() {
   test('getPlatformVersion', () async {
     MockHaishinKitPlatform fakePlatform = MockHaishinKitPlatform();
     HaishinKitPlatformInterface.instance = fakePlatform;
-
     // expect(await haishinKitPlugin.getPlatformVersion(), '42');
   });
 }
